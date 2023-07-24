ff8f35ec6318bc08c214bb34841b6d754f8c3eda
MaratFront <115290688+MaratFront@users.noreply.github.com>
2023-07-24 15:02:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -41,11 +41,11 @@ async function DataTable(config) {
   postPut.appendChild(postInputTh);
   postPut.appendChild(postBtn);
   parentObj.tr.appendChild(postPut);
-  let td1 = document.createElement('td');
+  let td1=document.createElement('td');
   td1.classList.add('row');
   Object.entries(dataArray.data).map(([key, item]) => {
     const tr = document.createElement('tr');
-    const td1 = document.createElement('td');
+    const td1 = document.createElement('td')
     td1.innerHTML = parentObj.index++;
     tr.appendChild(td1);
 
@@ -89,13 +89,13 @@ async function DataTable(config) {
     });
   });
 
-  let counter = dataArray.length + 2;
+  let counter = dataArray.length+2;
 
   function updateRowNumbers() {
     const rows = parentObj.tbody.querySelectorAll('tr');
     rows.forEach((row, index) => {
       const tdIndex = row.children[0];
-      tdIndex.innerHTML = index + 1;
+      tdIndex.innerHTML = index+1;
     });
   }
   postBtn.addEventListener('click', async () => {
@@ -104,11 +104,10 @@ async function DataTable(config) {
     const tdInput1 = document.createElement('td');
     const tdInput2 = document.createElement('td');
     const tdInput3 = document.createElement('td');
-
+    
     // Set the correct index for each row when adding a new record
-    tdIndex.innerHTML = counter;
+    tdIndex.innerHTML=counter;
     counter++;
-    updateRowNumbers();
     const postInput1 = document.createElement('input');
     const postInput2 = document.createElement('input');
     const postInput3 = document.createElement('input');
@@ -123,72 +122,56 @@ async function DataTable(config) {
     tr.appendChild(tdInput3);
 
     parentObj.tbody.insertBefore(tr, parentObj.tbody.firstChild);
+    updateRowNumbers();
+    postInput3.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        const newRecord = {
+          name: postInput1.value,
+          surname: postInput2.value,
+          birthday: postInput3.value,
+        };
 
-    postInput3.addEventListener('keydown', async (event) => {
-     // const newData = parentObj.api; // This should be the correct endpoint for creating new records
+        if (postInput1.value === '' || postInput2.value === '' || postInput3.value === '') {
+          postInput1.style.border = postInput1.value === '' ? '1px solid red' : '';
+          postInput2.style.border = postInput2.value === '' ? '1px solid red' : '';
+          postInput3.style.border = postInput3.value === '' ? '1px solid red' : '';
+        } else {
+          addRecordToTable(newRecord, tr);
+          tdInput1.style.display = 'none';
+          tdInput2.style.display = 'none';
+          tdInput3.style.display = 'none';
 
-      try {
-        if (event.key === 'Enter') {
-          const newRecord = {
-            name: postInput1.value,
-            surname: postInput2.value,
-            birthday: postInput3.value,
-          };
+          const tdButton = document.createElement('td');
+          const deleteButton = document.createElement('button');
+          deleteButton.classList.add('delBtn');
+          deleteButton.innerHTML = 'Видалити';
+          tdButton.appendChild(deleteButton);
+          tr.appendChild(tdButton);
 
-          if (postInput1.value === '' || postInput2.value === '' || postInput3.value === '') {
-            postInput1.style.border = postInput1.value === '' ? '1px solid red' : '';
-            postInput2.style.border = postInput2.value === '' ? '1px solid red' : '';
-            postInput3.style.border = postInput3.value === '' ? '1px solid red' : '';
-          } else {
-            const postResponse = await fetch(parentObj.api, {
-              method: 'POST',
-              body: Object.entries(newRecord), 
-              headers: {
-                'Content-Type': 'application/json',
-              },
-            });
+          for (const key of Object.keys(dataArray.data)) {
+            deleteButton.addEventListener('click', async () => {
+              try {
+                const deleteResponse = await fetch(`${parentObj.api}/${key}`, {
+                  method: 'DELETE',
+                });
 
-            if (postResponse.ok) {
-              // Assuming the response contains the newly created record data, you can update the table with it
-              const responseData = await postResponse.json();
-              addRecordToTable(responseData, tr);
-
-              // Hide the input fields and show the delete button for the new record
-              tdInput1.style.display = 'none';
-              tdInput2.style.display = 'none';
-              tdInput3.style.display = 'none';
-
-              const tdButton = document.createElement('td');
-              const deleteButton = document.createElement('button');
-              deleteButton.classList.add('delBtn');
-              deleteButton.innerHTML = 'Видалити';
-              tdButton.appendChild(deleteButton);
-              tr.appendChild(tdButton);
-
-              deleteButton.addEventListener('click', async () => {
-                try {
-                  const deleteResponse = await fetch(`${parentObj.api}/${responseData.id}`, {
-                    method: 'DELETE',
-                  });
-
-                  if (deleteResponse.ok) {
-                    tr.style.display = 'none';
-                  } else {
-                    throw new Error('Delete request failed');
-                  }
-                } catch (error) {
-                  console.error(error);
+                if (deleteResponse.ok) {
+                  tr.style.display = 'none';
+                } else {
+                  throw new Error('Delete request failed');
                 }
-              });
-            } else {
-              throw new Error('Post request failed');
-            }
+              } catch (error) {
+                console.error(error);
+              }
+            });
           }
         }
-      } catch (error) {
-        console.error(error);
-      }
-    });
+      }else {
+      postInput1.style.border = '';
+      postInput2.style.border = '';
+      postInput3.style.border = '';
+    }
+    })
   });
 
   function addRecordToTable(record, tr) {
@@ -216,12 +199,9 @@ const config1 = {
     { title: 'Прізвище', value: 'surname' },
     { title: 'Вік', value: 'birthday' },
   ],
-  apiUrl: 'https://mock-api.shpp.me/mlv/users', // замініть nsurname на своє ім'я та прізвище
-};
-
+  apiUrl: 'https://mock-api.shpp.me/mllv/users', // замініть nsurname на своє ім'я та прізвище
+}
 DataTable(config1);
-
-
 
 
 
